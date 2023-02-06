a1ade8639fb9cb59cbb926619413c63005b7a531
Asus <emirand20>
2023-02-06 18:46:36 +0100

    hooksJavi

---
+++ Miranda_Balladares_Javier/HOOKS/ex1.jsx
@@ -0,0 +1,13 @@
+import Titol from "./Components/Titol";
+import NomProfessio from "./Components/Nomprofessio";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Titol />
+    </div>
+  );
+}
+
+export default App;
